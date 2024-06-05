d6583a4a8604e4031a331ae8ac9f58178db652a5
raininsnovv <raininsnovv@gmail.com>
2024-06-05 03:53:58 +0300

    add funcs for get addresses from user

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from config import TELEGRAM_TOKEN
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
+user_addresses = {}
+
 
 @bot.message_handler(commands=["help", "start"])
 def send_welcome(message):
@@ -13,6 +15,31 @@ def send_welcome(message):
         f"Отправь мне пункт назначения, и я помогу тебе объехать пробки."
     )
     bot.reply_to(message, text)
+    chat_id = message.chat.id
+    msg = bot.send_message(chat_id, 'Введите адрес вашего дома')
+    bot.register_next_step_handler(msg, get_user_address_home)
+
+
+def get_user_address_home(message):
+    user_id = message.from_user.id
+    home_address = message.text
+    user_addresses[user_id] = {
+        'home': home_address
+    }
+
+    chat_id = message.chat.id
+    msg = bot.send_message(chat_id, 'Введите адрес вашей работы')
+    bot.register_next_step_handler(msg, get_user_address_job)
+
+
+def get_user_address_job(message):
+    user_id = message.from_user.id
+    job_address = message.text
+    user_addresses[user_id]['job'] = job_address
+
+    chat_id = message.chat.id
+    bot.send_message(chat_id, 'Спасибо. Информация сохранена.')
+    print(user_addresses)
 
 
 def main():
